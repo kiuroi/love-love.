626d853501478b5fff55b8024c23d6b87452cad7
kiuroi <tellessarah3@gmail.com>
2025-03-06 00:04:15 -0400

    Actualizar scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 document.getElementById('openButton').addEventListener('click', function() {
     document.getElementById('envelopeFlap').classList.toggle('open');
     setTimeout(() => {
-        document.querySelector
+        document.querySelector('.envelope-body h2').style.display = 'none';
+        document.querySelector('.envelope-body img').classList.add('show');
+    }, 600); // Time it takes for the flap to open
+});
